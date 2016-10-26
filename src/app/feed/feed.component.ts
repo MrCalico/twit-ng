3a2c15c01884186485cb1ead4a89ebbc795c1f29
John <John@ApexMicro.com>
2016-10-26 06:57:20 -0700

    left open files

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -20,12 +20,6 @@ export class FeedComponent implements OnInit {
     this.tweets = this.feedService.getCurrentFeed();
   }
 
-  private isUserInCollection(collection : string[], userId : string) : boolean {
-
-    return collection.indexOf(userId) != -1;
-
-  }
-
 OnFavorite(tweet) {
   this.feedService.favoriteTweet(tweet);
 }
