4a1fbf49f73d71442059fb7c359e35b935967ad0
John <John@ApexMicro.com>
2016-10-29 16:23:05 -0700

    to lowercase

--- src/app/tweet.ts
+++ src/app/tweet.ts
@@ -4,7 +4,7 @@ export class Tweet {
    
     constructor(public id: number, public body : string, public author: string, public date: Date, 
                 public retweets: Array<string>, public favorites: Array<string>) {
-        this.avatar = `${author}.jpg`;
+        this.avatar = `${author.toLowerCase()}.jpg`;
     }
    
     hasFavorited(userId : string) : boolean {
